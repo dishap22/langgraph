d1dcea57a390834884198d5da2e650616655f690
dishap22 <dishapant22@gmail.com>
2025-05-28 10:18:45 +0530

    basic llm interface

---
+++ agents/Agent_Bot.py
@@ -0,0 +1,35 @@
+from typing import TypedDict, List
+from langchain_core.messages import HumanMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: List[HumanMessage]
+
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
+
+def process(state: AgentState) -> AgentState:
+    """
+    Process the state by sending the messages to the LLM and updating the state with the response.
+    """
+    if not state['messages']:
+        return state
+
+    response = llm.invoke(state['messages'])
+    print(f"\nAI: {response.content}")
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+
+agent = graph.compile()
+
+user_input = input("Enter: ")
+while user_input != "exit":
+    agent.invoke({"messages": [HumanMessage(content=user_input)]})
+    user_input = input("Enter: ")
